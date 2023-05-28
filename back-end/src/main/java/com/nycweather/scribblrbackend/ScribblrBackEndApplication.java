fa18f4b848f00ef40b69f7498d2476b7362992b3
nycweather <alifftw@gmail.com>
2023-05-27 21:13:08 -0400

    initial commit

---
+++ back-end/src/main/java/com/nycweather/scribblrbackend/ScribblrBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.nycweather.scribblrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribblrBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribblrBackEndApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/nycweather/scribblrbackend/ScribblrBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nycweather.scribblrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribblrBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
